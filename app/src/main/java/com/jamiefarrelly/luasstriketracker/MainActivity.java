e36583edf5def2ededa217941e1af3dc428dbaea
Jamie Farrelly <jamie.farrelly@paywithfire.com>
2016-05-08 15:33:52 +0100

    cleanup, removing options menu

--- app/src/main/java/com/jamiefarrelly/luasstriketracker/MainActivity.java
+++ app/src/main/java/com/jamiefarrelly/luasstriketracker/MainActivity.java
@@ -6,8 +6,6 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -115,27 +113,5 @@ public class MainActivity extends AppCompatActivity {
             tvErrorMessage.setVisibility(View.VISIBLE);
         }
 
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
