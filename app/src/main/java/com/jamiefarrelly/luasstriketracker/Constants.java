3a8dabc025e6dd1c40fd90714f792cca887f8049
Jamie Farrelly <jamie.farrelly@paywithfire.com>
2016-05-08 14:34:44 +0100

    Basic version of app working, WIP

---
+++ app/src/main/java/com/jamiefarrelly/luasstriketracker/Constants.java
@@ -0,0 +1,9 @@
+package com.jamiefarrelly.luasstriketracker;
+
+/**
+ * Created by JamieF on 07/05/2016.
+ */
+public class Constants {
+
+    public static final String LOG = "LuasStrikeTracker";
+}
---
+++ app/src/main/java/com/jamiefarrelly/luasstriketracker/HttpUtils.java
@@ -0,0 +1,56 @@
+package com.jamiefarrelly.luasstriketracker;
+
+import android.os.AsyncTask;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+/**
+ * Created by JamieF on 07/05/2016.
+ */
+public class HttpUtils extends AsyncTask<Void, Void, String> {
+
+    private static final String API_URL = "https://brilliant-torch-1709.firebaseio.com/Info.json";
+
+    protected void onPreExecute() {
+        //progressBar.setVisibility(View.VISIBLE);
+        //responseView.setText("");
+    }
+
+    protected String doInBackground(Void... urls) {
+
+        try {
+            URL url = new URL(API_URL);
+            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+            try {
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+                StringBuilder stringBuilder = new StringBuilder();
+                String line;
+                while ((line = bufferedReader.readLine()) != null) {
+                    stringBuilder.append(line).append("\n");
+                }
+                bufferedReader.close();
+                return stringBuilder.toString();
+            }
+            finally{
+                urlConnection.disconnect();
+            }
+        }
+        catch(Exception e) {
+            Log.e(Constants.LOG, e.getMessage(), e);
+            return null;
+        }
+    }
+
+    protected void onPostExecute(String response) {
+        if(response == null) {
+            response = "THERE WAS AN ERROR";
+        }
+        //progressBar.setVisibility(View.GONE);
+        Log.i(Constants.LOG, response);
+        //responseView.setText(response);
+    }
+}
